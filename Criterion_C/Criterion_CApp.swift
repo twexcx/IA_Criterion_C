595c0b61965681bd8e39d74d31ddcefea46e3915
twexcx <timmos1009@mail.ru>
2025-08-28 12:25:07 +0100

    origin

---
+++ Criterion_C/Criterion_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  Criterion_CApp.swift
+//  Criterion_C
+//
+//  Created by Twexcx on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Criterion_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
